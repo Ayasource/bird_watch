7e05da305f5d6ec225ebbad64150f7521ca9b71b
ayasource <ayana-aac@hotmail.co.uk>
2026-01-13 10:31:40 +0000

    Install bird app

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 
-if os.path.isfile(‘env.py’):
+if os.path.isfile("env.py"):
     import env
 
-SECRET_KEY = os.environ.get(‘SECRET_KEY’)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bird_watch',
 ]
 
 MIDDLEWARE = [
