1786a457025ba0bc6930d4d72c6779d3d039d467
ayasource <ayana-aac@hotmail.co.uk>
2026-01-13 16:27:24 +0000

    CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -32,11 +32,14 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 # Application definition
 
 INSTALLED_APPS = [
