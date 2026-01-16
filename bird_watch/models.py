39e19fb5e66ed391ddfb61905b3fc97c1a52ef1f
ayasource <ayana-aac@hotmail.co.uk>
2026-01-16 21:03:17 +0000

    Fixed slug bug

--- bird_watch/models.py
+++ bird_watch/models.py
@@ -6,11 +6,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Bird(models.Model):
     bird_name = models.CharField(max_length=200, null=False, blank=False)
-    bird_count = models.PositiveIntegerField(unique=False)
+    slug = models.SlugField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bird_watch_posts")
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    bird_count = models.PositiveIntegerField(unique=False)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
