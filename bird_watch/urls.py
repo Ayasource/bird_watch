e489a530fe2c04dc92182544a073e114e97aa34d
ayasource <ayana-aac@hotmail.co.uk>
2026-01-20 19:58:11 +0000

    Made home page the landing page with html

--- bird_watch/urls.py
+++ bird_watch/urls.py
@@ -2,12 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.home_page, name='home'),
-    path("", views.BirdEntry.as_view(), name=''),
+    path('', views.home_page, name='home'),
+    path('birds/', views.BirdEntry.as_view(), name='bird_list'),
     path('add-bird/', views.add_bird, name='add_bird'),
     path('profile/', views.user_profile, name='profile'),
     path('<slug:slug>/', views.bird_entry, name='bird_entry'),
     path('bird/<slug:slug>/edit/', views.bird_edit, name='bird_edit'),
     path('bird/<slug:slug>/delete/', views.bird_delete, name='bird_delete'),
-
 ]
