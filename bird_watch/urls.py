168bc9de79f1baeb7bbbab1786d74644095775e7
ayasource <ayana-aac@hotmail.co.uk>
2026-01-20 21:51:48 +0000

    Add delete warning and removed extra add button

--- bird_watch/urls.py
+++ bird_watch/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('birds/', views.user_bird_list, name='bird_list'),  # Changed
+    path('birds/', views.user_bird_list, name='bird_list'), 
     path('add-bird/', views.add_bird, name='add_bird'),
     path('profile/', views.user_profile, name='profile'),
     path('<slug:slug>/', views.bird_entry, name='bird_entry'),
