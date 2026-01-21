eefa96cfb36512f2ec9aac804f994b4356a69ee1
ayasource <ayana-aac@hotmail.co.uk>
2026-01-21 11:22:40 +0000

    Add validator for bird names

--- bird_watch/models.py
+++ bird_watch/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 STATUS = ((1, "Published"),)
 
 
+alpha_validator = RegexValidator(
+    regex=r'^[a-zA-Z\s]+$',
+    message='Bird name can only contain letters and spaces.'
+)
+
+
 class Bird(models.Model):
-    bird_name = models.CharField(max_length=200, null=False, blank=False)
+    bird_name = models.CharField(
+        max_length=200, 
+        null=False, 
+        blank=False,
+        validators=[alpha_validator]
+    )
     slug = models.SlugField(max_length=200, unique=False, default='')
     date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
